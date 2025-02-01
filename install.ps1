4ac1083da2a569f756666459de6f243f3b5b597b
TXG0Fk3 <leovertonx12@outlook.com>
2025-02-01 02:00:27 -0300

    Added Install Script

---
+++ install.ps1
@@ -0,0 +1,10 @@
+# --> https://github.com/TXG0Fk3 <-- #
+
+# Windows Post-Install Script for my Windows-Dots
+
+
+# Installing Main Softwares
+winget install amn.yasb LGUG2Z.komorebi LGUG2Z.whkd JanDeDobbeleer.OhMyPosh Microsoft.Powershell Fastfetch
+
+# Loading Config Files
+    #TODO
