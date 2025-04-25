dcd53eb1249cad30d199914700b527cf188e5272
TXG0Fk3 <137466333+TXG0Fk3@users.noreply.github.com>
2025-04-24 23:17:06 -0300

    fix: fix oh-my-posh prompt

--- Config/Documents/WindowsPowerShell/profile.ps1
+++ Config/Documents/WindowsPowerShell/profile.ps1
@@ -1,3 +1,3 @@
 fastfetch -c minimal
 
-h-my-posh init pwsh --config "$env:POSH_THEMES_PATH\\catppuccin_mocha.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\\catppuccin_mocha.omp.json" | Invoke-Expression
